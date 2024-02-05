eef8ba55a17e91185fb9ee94d873564816a1806b
malekcoders <alomayri970@outlook.com>
2024-02-05 17:54:26 +0300

    init

---
+++ src/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    
+    // Create a binary file
+
+    // Write to the binary file, and close the file stream
+
+    // Read from the binary file, and close the file stream
+    
+    return 0;
+}
